692051849777cd9c5723308de604993bcb178c0f
ninhojb <ninhojb@bol.com.br>
2020-06-04 20:58:27 -0300

    [0008] Criado API,

--- backend/migrations/20200604154909_create_table_empresa.js
+++ backend/migrations/20200604154909_create_table_empresa.js
@@ -1,11 +1,10 @@
-
-exports.up = function(knex) {
-    return knex.schema.createTable('raw.empresa', table =>{
+exports.up = function (knex) {
+    return knex.schema.createTable('raw.empresa', table => {
         table.increments('id').primary()
         table.string('nome')
     })
 };
 
-exports.down = function(knex) {
+exports.down = function (knex) {
     return knex.schema.dropTable('raw.empresa')
 };
